54c130c1b5f5f5cc9acc94ceb5c938ef8238b6df
bouru02 <bouru02@gmail.com>
2022-07-20 19:25:09 +0900

    指摘があった点を修正

--- app.rb
+++ app.rb
@@ -7,23 +7,21 @@ require 'json'
 
 before '/*' do
   @memo_db_path = 'json/db.json'
-  @memo_db = JSON.parse(open(@memo_db_path).read)
+  @memo_db = JSON.parse(File.open(@memo_db_path).read)
   @all_memos = @memo_db['memos']
 end
 
+def get_the_memo(memo_id)
+  @all_memos.find { |memo| memo['id'].to_s == memo_id.to_s }
+end
+
+def update_memos
+  File.open(@memo_db_path, 'w') do |file|
+    JSON.dump(@memo_db, file)
+  end
+end
+
 helpers do
-  def get_the_memo(memo_id)
-    @all_memos.each do |memo|
-      return memo if memo['id'].to_s == memo_id.to_s
-    end
-  end
-
-  def update_memos
-    File.open(@memo_db_path, 'w') do |file|
-      JSON.dump(@memo_db, file)
-    end
-  end
-
   def escape(text)
     Rack::Utils.escape_html(text)
   end
@@ -33,13 +31,13 @@ get '/' do
   erb :index
 end
 
-get '/form' do
-  erb :form
+get '/memos/new' do
+  erb :memo_new
 end
 
-post '/form' do
+post '/memos' do
   new_memo_id = @all_memos.length + 1
-  new_memo = { 'id' => new_memo_id, 'title' => escape(params[:title]), 'content' => escape(params[:content]) }
+  new_memo = { id: new_memo_id, title: params[:title], content: params[:content] }
   @all_memos.push(new_memo)
   update_memos
   redirect to('/')
@@ -59,7 +57,7 @@ delete '/memos/:id' do
 end
 
 patch '/memos/:id' do
-  new_memo = { 'id' => params[:id], 'title' => escape(params[:title]), 'content' => escape(params[:content]) }
+  new_memo = { id: params[:id], title: params[:title], content: params[:content] }
   target_memo_index = params[:id].to_i - 1
   @all_memos[target_memo_index] = new_memo
   update_memos
