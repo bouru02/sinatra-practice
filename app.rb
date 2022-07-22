b8049619f1554df6bb1af25e232b107af9e0578c
bouru02 <bouru02@gmail.com>
2022-07-22 19:04:36 +0900

    DB版の大枠完成

--- app.rb
+++ app.rb
@@ -13,18 +13,12 @@ configure do
 end
 
 before '/*' do
-  @memo_db = setting.db
-  @all_memos = @memo_db.exec('SELECT * FROM memo ORDER BY created_at;')
+  @memo_db = settings.db
+  @all_memos = @memo_db.exec('SELECT * FROM memo ORDER BY id;')
 end
 
 def get_the_memo(memo_id)
   @all_memos.find { |memo| memo['id'].to_s == memo_id.to_s }
-end
-
-def update_memos
-  File.open(@memo_db_path, 'w') do |file|
-    JSON.dump(@memo_db, file)
-  end
 end
 
 helpers do
@@ -42,10 +36,7 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  new_memo_id = @all_memos.length + 1
-  new_memo = { id: new_memo_id, title: params[:title], content: params[:content] }
-  @all_memos.push(new_memo)
-  update_memos
+  @memo_db.exec("INSERT into memo (title,content) values ('#{params[:title]}','#{params[:content]}');")
   redirect to('/')
 end
 
@@ -55,18 +46,13 @@ get '/memos/:id' do
 end
 
 delete '/memos/:id' do
-  @all_memos.delete_if do |memo|
-    memo['id'].to_s == params[:id]
-  end
-  update_memos
+  @memo_db.exec("DELETE from memo WHERE id = #{params[:id]};")
   redirect to('/')
 end
 
 patch '/memos/:id' do
   new_memo = { id: params[:id], title: params[:title], content: params[:content] }
-  target_memo_index = params[:id].to_i - 1
-  @all_memos[target_memo_index] = new_memo
-  update_memos
+  @memo_db.exec("UPDATE memo SET title = '#{new_memo[:title]}', content = '#{new_memo[:content]}' WHERE id = #{new_memo[:id]};")
   redirect to('/')
 end
 
